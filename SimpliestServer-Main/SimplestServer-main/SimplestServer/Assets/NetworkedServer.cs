70236538d0b3b0e9957b9272686d1c010a289d36
Nathan Nguyen <zhectx@gmail.com>
2021-10-04 17:06:10 -0400

    Removing Library

--- SimpliestServer-Main/SimplestServer-main/SimplestServer/Assets/NetworkedServer.cs
+++ SimpliestServer-Main/SimplestServer-main/SimplestServer/Assets/NetworkedServer.cs
@@ -8,22 +8,33 @@ using UnityEngine.UI;
 
 public class NetworkedServer : MonoBehaviour
 {
-    int maxConnections = 1000;
+    const int maxConnections = 1000;
     int reliableChannelID;
     int unreliableChannelID;
     int hostID;
-    int socketPort = 5491;
+    const int socketPort = 5491;
+
+    LinkedList<PlayerAccount> playerAccounts;
 
     // Start is called before the first frame update
     void Start()
     {
+
         NetworkTransport.Init();
+
         ConnectionConfig config = new ConnectionConfig();
+
         reliableChannelID = config.AddChannel(QosType.Reliable);
         unreliableChannelID = config.AddChannel(QosType.Unreliable);
+
         HostTopology topology = new HostTopology(config, maxConnections);
+
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
-        
+
+
+        playerAccounts = new LinkedList<PlayerAccount>();
+        //We need to load our saved player accounts.
+
     }
 
     // Update is called once per frame
@@ -37,6 +48,7 @@ public class NetworkedServer : MonoBehaviour
         int bufferSize = 1024;
         int dataSize;
         byte error = 0;
+
 
         NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostID, out recConnectionID, out recChannelID, recBuffer, bufferSize, out dataSize, out error);
 
@@ -56,18 +68,118 @@ public class NetworkedServer : MonoBehaviour
                 break;
         }
 
+
     }
-  
+
     public void SendMessageToClient(string msg, int id)
     {
         byte error = 0;
         byte[] buffer = Encoding.Unicode.GetBytes(msg);
         NetworkTransport.Send(hostID, id, reliableChannelID, buffer, msg.Length * sizeof(char), out error);
     }
-    
+
     private void ProcessRecievedMsg(string msg, int id)
     {
         Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
+
+        string[] csv = msg.Split(',');
+
+        int signifier = int.Parse(csv[0]);
+
+        if (signifier == ClientToServerSignifiers.CreateAccount)
+        {
+            string n = csv[1];
+            string p = csv[2];
+
+            bool isUnique = false;
+
+            foreach (PlayerAccount pa in playerAccounts)
+            {
+                if (pa.name == n)
+                {
+                    isUnique = true;
+                    break;
+                }
+            }
+
+            if (!isUnique)
+            {
+                playerAccounts.AddLast(new PlayerAccount(n, p));
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.Success, id);
+            }
+            else
+            {
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureNameInUse, id);
+            }
+        }
+        else if (signifier == ClientToServerSignifiers.Login)
+        {
+            string n = csv[1];
+            string p = csv[2];
+
+            bool hasBeenFound = false;
+
+            foreach (PlayerAccount pa in playerAccounts)
+            {
+                if (pa.name == n)
+                {
+                    
+                    if(pa.password == p)
+                    {
+                        SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.Success, id);
+                    }
+                    else
+                    {
+                        SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureIncorrectPassword, id);
+                    }
+
+                    hasBeenFound = true;
+                    break;
+                }
+            }
+
+            if(!hasBeenFound)
+            {
+                SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureNameNotFound, id);
+            }
+
+        }
+
     }
 
+}
+
+
+public class PlayerAccount
+{
+    public string name, password;
+
+    public PlayerAccount(string Name, string Password)
+    {
+        name = Name;
+        password = Password;
+    }
+}
+
+
+
+
+public static class ClientToServerSignifiers
+{
+    public const int Login = 1;
+    public const int CreateAccount = 2;
+}
+
+public static class ServerToClientSignifiers
+{
+    public const int LoginResponse = 1;
+
+}
+
+public static class LoginResponses
+{
+    public const int Success = 1;
+    public const int FailureNameInUse = 2;
+    public const int FailureNameNotFound = 3;
+    public const int FailureIncorrectPassword = 4;
 }
