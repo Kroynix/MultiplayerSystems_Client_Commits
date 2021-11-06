fbd00515c91bde08d75031eab7c8a07b1af5e748
Nathan Nguyen <zhectx@gmail.com>
2021-11-06 18:41:29 -0400

    Attempt at adding User Connected List

--- SimpliestServer-Main/SimplestServer-main/SimplestServer/Assets/NetworkedServer.cs
+++ SimpliestServer-Main/SimplestServer-main/SimplestServer/Assets/NetworkedServer.cs
@@ -14,12 +14,17 @@ public class NetworkedServer : MonoBehaviour
     int hostID;
     int socketPort = 3389;
 
+    List<int> idlist;
+    List<IDName> idNameLink;
+
     LinkedList<PlayerAccount> playerAccounts;
     string playerAccountFilePath;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        Debug.Log("Started Server");
         // "Kind of a constant"
         playerAccountFilePath = Application.dataPath + Path.DirectorySeparatorChar + "PlayerAccountData.txt";
 
@@ -31,7 +36,11 @@ public class NetworkedServer : MonoBehaviour
         HostTopology topology = new HostTopology(config, maxConnections);
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
 
+        // List of Player accounts and current connected ID's
         playerAccounts = new LinkedList<PlayerAccount>();
+        idlist = new List<int>();
+        idNameLink = new List<IDName>();
+
         //We need to load our saved Player Accounts.
         LoadPlayerAccounts();
         
@@ -56,6 +65,16 @@ public class NetworkedServer : MonoBehaviour
                 break;
             case NetworkEventType.ConnectEvent:
                 Debug.Log("Connection, " + recConnectionID);
+                idlist.Add(recConnectionID);
+
+                /*
+                foreach(IDName identity in idNameLink)
+                {
+                    if (identity.id != recConnectionID)
+                        SendMessageToClient(ChatStates.ConnectedUserList + "," + identity.name + "," + identity.id, identity.id);
+                }
+                */
+
                 break;
             case NetworkEventType.DataEvent:
                 string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
@@ -64,12 +83,14 @@ public class NetworkedServer : MonoBehaviour
                 break;
             case NetworkEventType.DisconnectEvent:
                 Debug.Log("Disconnection, " + recConnectionID);
+                idlist.Remove(recConnectionID);
                 break;
         }
 
     }
   
-    public void SendMessageToClient(string msg, int id) {
+    public void SendMessageToClient(string msg, int id) 
+    {
         byte error = 0;
         byte[] buffer = Encoding.Unicode.GetBytes(msg);
         NetworkTransport.Send(hostID, id, reliableChannelID, buffer, msg.Length * sizeof(char), out error);
@@ -82,6 +103,8 @@ public class NetworkedServer : MonoBehaviour
         string[] csv = msg.Split(',');
 
         int signifier = int.Parse(csv[0]);
+
+
 
         if(signifier == ClientToServerSignifiers.CreateAccount)
         {
@@ -119,7 +142,7 @@ public class NetworkedServer : MonoBehaviour
             string p = csv[2];
 
             bool hasBeenFound = false;
-            //bool responseHasBeenSent = false;
+            
 
             foreach(PlayerAccount pa in playerAccounts)
             {
@@ -128,12 +151,22 @@ public class NetworkedServer : MonoBehaviour
                     if(pa.Password == p)
                     {
                         SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.Success + "," + "You have Successfully Logged In", id);
-                        //responseHasBeenSent = true;
+                        SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.SendUsername + "," + n, id);
+                        /*
+                        foreach (int identifier in idlist)
+                        {
+                            SendMessageToClient(ChatStates.ConnectedUserList + "," + n + "," + id, id);
+                        }
+
+                        idNameLink.Add(new IDName(id, n));
+                        */
+                        
+
                     }
                     else
                     {
                         SendMessageToClient(ServerToClientSignifiers.LoginResponse + "," + LoginResponses.FailureIncorrectPassword + "," + "Incorrect Password", id);
-                        //responseHasBeenSent = true;
+                        
                     }
                     hasBeenFound = true;
                     break;
@@ -146,7 +179,23 @@ public class NetworkedServer : MonoBehaviour
             }
 
         }
-        else
+
+        else if (signifier == ChatStates.ClientToServer)
+        {
+            string name = csv[1];
+            string message = csv[2];
+            foreach (int identifier in idlist)
+            {
+                SendMessageToClient(ChatStates.ServerToClient + "," + name + "," + message, identifier);
+            }
+        }
+
+        else if (signifier == ClientToServerSignifiers.FindMatch)
+        {
+            Debug.Log("User trying to find match");
+        }
+
+        else if (signifier == ClientToServerSignifiers.AddToGameSeesion)
         {
 
         }
@@ -193,6 +242,20 @@ public class GameSession
 }
 
 
+public class IDName
+{
+    public int id;
+    public string name;
+
+    public IDName(int identifitier, string Name)
+    {
+        id = identifitier;
+        name = Name;
+    }
+
+}
+
+
 
 public class PlayerAccount
 {
@@ -206,9 +269,12 @@ public class PlayerAccount
 }
 
 
+
 public static class ClientToServerSignifiers{
     public const int Login = 1;
     public const int CreateAccount = 2;
+    public const int FindMatch = 3;
+    public const int AddToGameSeesion = 4;
 }
 
 public static class ServerToClientSignifiers{
@@ -223,6 +289,14 @@ public static class LoginResponses{
     public const int FailureNameNotFound = 3;
     public const int FailureIncorrectPassword = 4;
     public const int AccountCreated = 5;
+    public const int SendUsername = 6;
+}
+
+public static class ChatStates
+{
+    public const int ClientToServer = 7;
+    public const int ServerToClient = 8;
+    public const int ConnectedUserList = 9;
 }
 
 
